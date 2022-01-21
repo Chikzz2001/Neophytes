46a8e0779f711693dae616314006252d8cc13083
Chikzz2001 <88380455+Chikzz2001@users.noreply.github.com>
2022-01-21 16:33:39 +0530

    Add files via upload

---
+++ Explore/script.js
@@ -0,0 +1,5 @@
+gsap.from('.header',{duration: 2,opacity:0})
+gsap.from('.info',{duration: 2,opacity:0})
+gsap.from('.card1',{duration: 1.5, x: '-100vw',delay:1.5})
+gsap.from('.card2',{duration: 1.5, x: '200vw', delay: 3})
+gsap.from('.card3',{duration: 1.5, x: '-100vw',delay: 4.5})
